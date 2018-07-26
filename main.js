7b932d1c89648df50f935d57725e956868349328
David Scott Bowen <davidscottbowen@gmail.com>
2018-07-26 09:35:07 -0400

    added register

--- main.js
+++ main.js
@@ -1,34 +1,47 @@
 function login() {
-    var userEmail = document.getElementById("email").value;
-    var userPassword = document.getElementById("password").value;
+  var userEmail = document.getElementById("email").value;
+  var userPassword = document.getElementById("password").value;
 
-    firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        
-        windows.alert("Error : " + errorMessage);
-      });
-    
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    windows.alert("Error : " + errorMessage);
+  });
+
 }
 
 function logout() {
   firebase.auth().signOut()
 }
 
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      document.getElementById("bottom").style.display = "block";
-      document.getElementById("top").style.display = "none";
+function register() {
+  var userEmail = document.getElementById("email").value;
+  var userPassword = document.getElementById("password").value;
 
-      var user = firebase.auth().currentUser;
-
-      if(user != null) {
-        var email_id = user.email;
-        document.getElementById("userInfo").innerHTML = email_id;
-      }
-    } else {
-        document.getElementById("bottom").style.display = "none";
-        document.getElementById("top").style.display = "block";
-    }
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
   });
+}
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // document.getElementById("bottom").style.display = "block";
+    // document.getElementById("top").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if (user != null) {
+      var email_id = user.email;
+      document.getElementById("userInfo").innerHTML = email_id;
+    }
+  } else {
+    document.getElementById("userInfo").innerHTML = "";
+    // document.getElementById("bottom").style.display = "none";
+    // document.getElementById("top").style.display = "block";
+  }
+});
